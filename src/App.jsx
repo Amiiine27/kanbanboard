9da405db71b9ffa49eada1a7bc2255e1fe2f8886
Amiiine27 <amineakh213@outlook.fr>
2025-08-21 03:40:02 +0200

    deplacer les cartes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,63 @@
 import "./App.css";
 import Column from "./components/Column";
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [kanbanData, setKanbanData] = useState({
+    todo: [
+      {
+        task: "Se laver les dents",
+        description:
+          "Se brosser les dents, passer le fil dentaire et le jet dentaire",
+      },
+      { task: "Aller a la salle de sport", description: "Séance pecs triceps" },
+      {
+        task: "Faire les courses",
+        description: "Acheter du pain, du lait, des oeufs",
+      },
+    ],
+    inProgress: [
+      {
+        task: "Apprendre React",
+        description: "Apprendre React pour créer des applications web",
+      },
+    ],
+    done: [],
+  });
 
-  var tasks = [
-    { task: "Se laver les dents", description: "Se brosser les dents, passer le fil dentaire et le jet dentaire" },
-    { task: "Aller a la salle de sport", description: "Séance pecs triceps"}
-  ]
+  const moveCard = (cardIndex, startColumn, endColumn) => {
+    // On recupere la carte a déplacer
+    const cardToMove = kanbanData[startColumn][cardIndex]
 
-  console.log(tasks);
+    // On supprime la carte de son emplacement actuel
+    const newStartColumnCards = kanbanData[startColumn].filter((card, index) => index!==cardIndex);
+
+    // Ajouter la carte à la colonne de destination  
+    const newEndColumnCards = [...kanbanData[endColumn], cardToMove];
+
+    setKanbanData({
+      ...kanbanData,
+      [startColumn]: newStartColumnCards,
+      [endColumn]: newEndColumnCards
+    });
+
+    
+
+    console.log("Function moveCard Appelée");
+  };
+
+  useEffect(() => {
+    moveCard(0, "todo", "inProgress");
+  }, []); // Le [] = "exécute une seule fois"
 
   return (
     <>
       <div className="min-h-screen max-h-screen bg-gray-100 p-4">
         <h1 className="text-2xl font-bold text-center mb-4">Tableau Kanban</h1>
         <div className="flex gap-4 max-w-7xl mx-auto">
-          <Column title="To Do" cardsTab={tasks}/>
-          <Column title="In Progress" cardsTab={tasks}/>
-          <Column title="Done" cardsTab={tasks}/>
+          <Column title="To Do" cardsTab={kanbanData.todo} />
+          <Column title="In Progress" cardsTab={kanbanData.inProgress} />
+          <Column title="Done" cardsTab={kanbanData.done} />
         </div>
       </div>
     </>
