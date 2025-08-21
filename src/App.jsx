a211a5683f9d7f0a2304eaad2ebd11a2022745ee
Amiiine27 <amineakh213@outlook.fr>
2025-08-21 02:43:03 +0200

    card loop done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import "./App.css";
 import Column from "./components/Column";
 
 function App() {
+
+  var tasks = [
+    { task: "Se laver les dents", description: "Se brosser les dents, passer le fil dentaire et le jet dentaire" },
+    { task: "Aller a la salle de sport", description: "Séance pecs triceps"}
+  ]
+
+  console.log(tasks);
+
   return (
     <>
       <div className="min-h-screen max-h-screen bg-gray-100 p-4">
         <h1 className="text-2xl font-bold text-center mb-4">Tableau Kanban</h1>
         <div className="flex gap-4 max-w-7xl mx-auto">
-          <Column title="To Do" />
-          <Column title="In Progress" />
-          <Column title="Done" />
+          <Column title="To Do" cardsTab={tasks}/>
+          <Column title="In Progress" cardsTab={tasks}/>
+          <Column title="Done" cardsTab={tasks}/>
         </div>
       </div>
     </>
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,7 +1,12 @@
-export default function Column({title}) {
+import Card from "./Card";
+
+export default function Column({title, cardsTab}) {
   return (
     <div className="w-1/3 bg-white rounded-lg min-h-96">
         <h1 className="text-xl font-bold p-4 border-b border-gray-200 text-center">{title}</h1>
+        {cardsTab.map((card, index) => (
+            <Card key={index} task={card.task} description={card.description} />
+        ))}
     </div>
   )
 }
